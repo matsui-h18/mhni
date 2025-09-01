d5c372ac3a62084a83b9f8b92b4fa313976c6014
HAYASHI <hayashi.ff@example.com>
2025-09-01 12:03:17 +0900

    コミットしました

--- app/Http/Controllers/AccController.php
+++ app/Http/Controllers/AccController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class AccController extends Controller
 {
-    //
+    public function store(Request $req){
+        $Book=new Book();
+        $Book->book_name=$req->book_name;
+        $Book->author=$req->author;
+        $Book->pub_date=$req->pub_date;
+        $Book->content=$req->price;
+        $Book->isbn=$req->isbn;
+        $Book->save();
+        $data=[
+            'book_name'=>$req->book_name,
+            'author'=>$req->author,
+            'pub_date'=>$req->pub_date,
+            'price'=>$req->price,
+            'isbn'=>$req->isbn];
+        return view('admin.newBookComplete',$data);
+
+    }
+    public function allshow(){
+        $data=[
+            'books'=>Book::all()
+        ];
+        return view('admin.index2',$data);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccController;
 
 Route::get('/', function () {
      return view('normal.bookDetail');
 });
 Route::get('/3', function () {
-    return view('admin.newBook');
+    return view('admin.newBookComplete');
 });
 
 Route::get('/1', function () {
     return view('normal.index');
 });
-Route::get('/2', function () {
+// Route::get('/2', function () {
+//     return view('admin.index2');
+// });
+Route::get('/admin/new-book', function () {
+    return view('admin.newBook');
+})->name('newBook');
+
+/*Route::get('/admin/index2', function () {
     return view('admin.index2');
-});
+})->name('admin.index2');*/
+
+
+//Route::get('/admin/newBookComplete', [AccController::class,'store'] ) ;
+Route::post('/admin/newBookComplete', [AccController::class,'store'] ) ;
+
+
+// こっちだけ残す！
+Route::get('/admin/index2', [AccController::class,'allshow'])->name('admin.index2');
+
+
