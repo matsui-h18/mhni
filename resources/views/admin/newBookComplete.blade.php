9418931d5b73274c9175a4e0b96d63a2ec3cca18
Matsui <m.honoka.act0912@gmail.com>
2025-09-01 21:01:19 +0900

    本の新規登録完了ページ
    CSSいじりました。

--- resources/views/admin/newBookComplete.blade.php
+++ resources/views/admin/newBookComplete.blade.php
@@ -1,19 +1,31 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-     <meta http-equiv="refresh" content="2;url={{ route('admin.index2') }}">
-
-</head>
-<body>
-    <h1>本の新規登録</h1>
-    <p>{{$book_name}}</p>
-    <p>{{$author}}</p>
-    <p>{{$pub_date}}</p>
-    <p>{{$isbn}}</p>
+@extends('layouts.headFoot')
+@section('title','本の新規登録')
+@section('main')
+<style>
 
 
-</body>
-</html>
+    h1{
+        text-align:center;
+        margin-top: 50px;
+        margin-bottom: 50px;
+    }
+
+    p{
+        text-align:center;
+        font-size:30px;
+    }
+
+    .newData{
+        margin:50px;
+    }
+</style>
+<meta http-equiv="refresh" content="2;url={{ route('admin.index2') }}">
+
+    <h1>新規登録が完了しました</h1>
+    <div class="newData">
+        <p>{{$book_name}}</p>
+        <p>{{$author}}</p>
+        <p>{{$pub_date}}</p>
+        <p>{{$isbn}}</p>
+    </div>
+@endsection
