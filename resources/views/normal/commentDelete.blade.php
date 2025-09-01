03213a0bb15d7e9fe134d737479d051ebb75985c
04iwamoto <haru-306@hotmail.co.jp>
2025-09-01 16:56:00 +0900

    9/1最終岩本

--- resources/views/normal/commentDelete.blade.php
+++ resources/views/normal/commentDelete.blade.php
@@ -8,15 +8,22 @@
 </head>
 <body>
     <h1>コメント削除</h1>
-    <form action="/nomal/commentDelete" method="post">
+    <p>本当に削除しますか？</p>
+
+    {{-- <form action="/nomal/commentDeleteComplete" method="post">
     @csrf
     <input type="hidden" name="id" value="{{ $record->id }}"readonly><br>
     本id{{$recprd ->id}} <br>
     投稿者<input type="text" name="user_name" value="{{ $record->user_name }}"readonly><br>
     コメント<textarea name="comment" readonly>{{$record->comment}}</textarea><br>
-    <input type="submit" value="削除">
-    <a href="/normal/bookDetail">本の詳細画面に戻る</a>
+    </form> --}}
+
+    <form action="/normal/commentDeleteComplete" method="post">
+        @csrf
+    <!-- 削除ボタン -->
+     {{-- <input type="hidden" name="comment_id" value="{{ $comment->id }}"> --}}
+    <input type="submit" value="削除" class="btn btn-danger">
     </form>
-    
+      
 </body>
 </html>
--- resources/views/normal/commentEditComplete.blade.php
+++ resources/views/normal/commentEditComplete.blade.php
@@ -13,14 +13,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>コメント投稿完了画面</title>
+    <meta http-equiv="refresh" content="2;url={{ route('normal.bookDetail') }}">
+    <title>コメント変更完了画面</title>
 </head>
 <body>
     <h1>コメント変更完了</h1>
-    <p>コメントが正常に登録されました。</p>//
+    <p>コメントが正常に変更されました。</p>//
     <h2><?= htmlspecialchars($result, ENT_QUOTES, 'UTF-8') ?></h2>
-    {{-- 2,3秒後に本の詳細画面に戻ります --}}
-    <a href="/normal/bookDetail">本の詳細画面に戻る</a>     
+    {{-- metaの中で2,3秒後に本の詳細画面normal.bookDetailに戻ります --}}    
 </body>
 </html>
