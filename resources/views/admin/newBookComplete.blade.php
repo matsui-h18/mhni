7d8ac1caaf21c6c41f2acf10daea8da0205ea874
YuuskeNakajima <yuu.rzsui@gmail.com>
2025-09-02 14:19:48 +0900

    本新規入力　修正

--- resources/views/admin/newBookComplete.blade.php
+++ resources/views/admin/newBookComplete.blade.php
@@ -2,7 +2,7 @@
 @section('title','本の新規登録')
 @section('main')
 <link rel="stylesheet" href="{{ asset('css/DeleteComplete and Ebit.css') }}">
-<meta http-equiv="refresh" content="2;url={{ route('admin.index2') }}">
+<meta http-equiv="refresh" content="2;url={{ route('support') }}">
 
     <h1>新規登録が完了しました</h1>
 
