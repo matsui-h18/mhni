b99c0a3cfd57dbf2adf6c8220642fc82d694dc54
HAYASHI <hayashi.ff@example.com>
2025-09-03 13:45:02 +0900

    微調整：林

--- resources/views/normal/commentEditComplete.blade.php
+++ resources/views/normal/commentEditComplete.blade.php
@@ -4,8 +4,8 @@
 <meta http-equiv="refresh" content="2;url={{ route('bookshow', ['id' => $book_id]) }}">
 <link rel="stylesheet" href="{{ asset('css/DeleteComplete and Ebit.css') }}">
     <h1>コメントが正常に変更されました。</h1>
-    <p>{{ $comment->user->emp_name }}</p>
-    <p>{{ $comment->comment }}</p>
+    <!-- <p>名前：{{ $comment->user->emp_name }}</p>
+    <p>コメント：{{ $comment->comment }}</p> -->
 
     {{-- metaの中で2,3秒後に本の詳細画面normal.bookDetailに戻ります --}}
 @endsection
