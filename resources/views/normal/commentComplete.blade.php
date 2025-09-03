b1a4d58fea8984d6bbbdf40069ae343620f328d9
HAYASHI <hayashi.ff@example.com>
2025-09-03 13:23:08 +0900

    直した

--- resources/views/normal/commentComplete.blade.php
+++ resources/views/normal/commentComplete.blade.php
@@ -2,7 +2,7 @@
 @section('title','コメント投稿完了画面')
 @section('main')
     <meta http-equiv="refresh" content="2;url={{ route('bookshow', ['id' => $book_id]) }}">
-   
+   <link rel="stylesheet" href="{{ asset('css/DeleteComplete and Ebit.css') }}">
     <h1>コメントが正常に登録されました。</h1>
     <p>{{ $comment_text }}</p>
 
--- resources/views/normal/commentEditComplete.blade.php
+++ resources/views/normal/commentEditComplete.blade.php
@@ -2,6 +2,7 @@
 @section('title','コメント変更完了画面')
 @section('main')
 <meta http-equiv="refresh" content="2;url={{ route('bookshow', ['id' => $book_id]) }}">
+<link rel="stylesheet" href="{{ asset('css/DeleteComplete and Ebit.css') }}">
     <h1>コメントが正常に変更されました。</h1>
     <p>{{ $comment->user->emp_name }}</p>
     <p>{{ $comment->comment }}</p>
